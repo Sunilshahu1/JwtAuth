230a462b3e5a99687616cf6c45f8405c2fa30a1b
Sunil Shahu <61456130+Sunilshahu1@users.noreply.github.com>
2022-09-14 09:46:55 +0530

    Add files via upload

---
+++ JWTAuth/JWTAuth.WebApi/Models/RefreshToken.cs
@@ -0,0 +1,10 @@
+﻿namespace JWTAuth.WebApi.Models
+{
+    public class RefreshToken
+    {
+        public string Token { get; set; }   = string.Empty;
+        public string RefreshTokenSecret { get; set; } =string.Empty;
+        public DateTime Created { get; set; }= DateTime.Now;
+        public DateTime Expires { get; set; }
+    }
+}
